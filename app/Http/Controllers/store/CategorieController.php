f8df7e8e65e79e233092ff4b0770f81087355343
Reda Mabchour <redamabshur@gmail.com>
2024-05-26 21:00:28 +0100

    CategorieController

---
+++ app/Http/Controllers/store/CategorieController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Store;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Categorie;
+
+class CategorieController extends Controller
+{
+    //
+    public function index()
+    {
+        $categories = Categorie::all();
+        return view('store.categories.index', compact('categories'));
+    }
+ 
+    public function create()
+    {
+        return view('store.categories.create');
+    }
+ 
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nom' => 'required|string|max:255',
+        ]);
+ 
+        Categorie::create($request->all());
+ 
+        return redirect()->route('categories.index')->with('success_message', 'Category created successfully.');
+    }
+ 
+    public function show(Categorie $category)
+    {
+        return view('store.categories.show', compact('category'));
+    }
+ 
+    public function edit(Categorie $category)
+    {
+        return view('store.categories.edit', compact('category'));
+    }
+ 
+    public function update(Request $request, Categorie $category)
+    {
+        $request->validate([
+            'nom' => 'required|string|max:255',
+        ]);
+ 
+        $category->update($request->all());
+ 
+        return redirect()->route('categories.index')->with('success_message', 'Category updated successfully.');
+    }
+ 
+    public function destroy(Categorie $category)
+    {
+        $category->delete();
+ 
+        return redirect()->route('categories.index')->with('success_message', 'Category deleted successfully.');
+    }
+}
