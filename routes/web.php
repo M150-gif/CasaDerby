84fa808846a4ecfd4e273775a854445dcc1ec36e
mostapha <moustapha1elhor@gmail.com>
2024-05-27 17:09:21 +0100

    set up the store

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,11 @@ use App\Http\Controllers\Store\CategorieController;
 use App\Http\Controllers\Store\Commandes_dashbord_controller;
 use App\Models\Product;
 
-//accuiel
+//accuiel//////////////////////////////////////////////////////////////////
 Route::get('/', function(){
     return view('accueil');
 })->name('home');
-//login_admins
+//login_admins///////////////////////////////////////////////////////////
 Route::middleware('guest')->group(function(){
     Route::get('/admin',[authentification::class,function(){
        return view('login_admin_page');}])->name('login_admin_page');
@@ -23,14 +23,13 @@ Route::middleware('guest')->group(function(){
         return view('login_admin_page');
         }])->name('login');
     });
-//logout_admins
+//logout_admins////////////////////////////////////////////////////////////
 Route::middleware(['auth','admins_logout'])->group(function () {
     Route::get('/logout_admins',[authentification::class,'logout_admins'])->name('logout_admins');
 });
-//admin_dashbord_store
+//admin_dashbord_store/////////////////////////////////////////////////////
         // midlleware_admin_store
     Route::middleware(['auth','admin_store'])->group(function () {
-
         Route::prefix('/admin_store')->group(function(){
             //crud_clients
             Route::controller(ClientCrudController::class)->group(function () {
@@ -49,30 +48,22 @@ Route::middleware(['auth','admins_logout'])->group(function () {
             Route::post('/profile/password', [Admin_store::class, 'updatePassword'])->name('profile.updatePassword');
         });
        });
-
 //Category
-
 Route::middleware(['auth', 'admin_store'])->prefix('admin_store')->group(function () {
     // Index
     Route::get('/categories', [CategorieController::class, 'index'])->name('categories.index');
-    
     // Create
     Route::get('/categories/create', [CategorieController::class, 'create'])->name('categories.create');
     Route::post('/categories', [CategorieController::class, 'store'])->name('categories.store');
-    
     // Show
     Route::get('/categories/{categorie}', [CategorieController::class, 'show'])->name('categories.show');
-
     // Delete
     Route::delete('/categories/{categorie}', [CategorieController::class, 'destroy'])->name('categories.destroy');
 });
-
-
-
-//store(site web)
+//store(site web)////////////////////////////////////////////////////////
 Route::prefix('/store')->group(function(){
     Route::get('/', function(){
-        return view('store/Home');
+        return view('store/site_web/user/login');
     })->name('store_home');
     //geust_client
         //middleware
@@ -88,7 +79,7 @@ Route::prefix('/store')->group(function(){
     })->name('display_profile');
     
 });
-//admin_dashbord_equiep_wac
+//admin_dashbord_equiep_wac////////////////////////////////////////////////
  // midlleware_admin_equipe_wac
     Route::middleware(['auth','admin_equipe_wac'])->group(function(){
     Route::controller(admin_equipe_wac::class)->group(function () {
@@ -97,7 +88,7 @@ Route::prefix('/store')->group(function(){
         });
     });
 });
-//admin_dashbord_equiep_rca
+//admin_dashbord_equiep_rca/////////////////////////////////////////////////
  // midlleware_admin_equipe_rca
  Route::middleware(['auth','admin_equipe_rca'])->group(function () {
     Route::controller(ClientCrudController::class)->group(function () {
@@ -106,11 +97,10 @@ Route::prefix('/store')->group(function(){
         });
     });
    });
-//equipe(site web)
+//equipe(site web)//////////////////////////////////////////////////////////////
 // Route::middleware('auth')->group(function () {
 
 // });
-
-Route::middleware('auth')->group(function(){
-});
-
+Route::get('/casadebrystore', function(){
+    return view('store/site_web/index');
+})->name('store_home');
