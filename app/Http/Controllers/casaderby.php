19cf319a66fa1ea0989e4c12c55ce345fa5ae53d
mostapha <moustapha1elhor@gmail.com>
2024-05-28 07:22:38 +0100

    new change

--- app/Http/Controllers/casaderby.php
+++ app/Http/Controllers/casaderby.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Categorie;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class casaderby extends Controller
 {
     public function get_categories(){
         $categories=Categorie::all();
-        return view('store.site_web.index',compact('categories'));
+        $products=Product::all();
+        return view('store.site_web.index',compact('categories','products'));
     }
 }
